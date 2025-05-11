be498377910b3448e3e1904a4f1152659733d620
alireza tabatabaeei poor <tabatabaeei.alireza@gmail.com>
2025-05-11 10:30:26 +0330

    define function renderTodolist and use them

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,6 +1,13 @@
 //roles button To exist in this app
 var addBtn = document.querySelector("#add");
 var inputValue = document.querySelector("#item");
+//define element related to html
+let container = document.querySelector(".container");
+let todo = document.querySelector(".todo");
+
+let completed = document.querySelector("#completed");
+
+/////////////////////////////تعریف تابع برای  اضافه کردن ایتم ها///////////////////////////////////////
 function addItem() {
   let itemText = inputValue.value;
 
@@ -15,16 +22,34 @@ function addItem() {
     inputValue.value = "";
   }
 }
+/////////////////////////////////////تعریف تابع برای اجرای عناصر لیست در صفحه///////////////////////////
+function renderTodolist() {
+  let todoList = JSON.parse(localStorage.getItem("todoItems")) || [];
+  let todoContainer = document.querySelector("#todo");
 
-// اجرای تابع با کلیک روی دکمه
-addBtn.addEventListener("click", addItem);
+  //پاک کردن لیست قبلی
+  todoContainer.innerHTML = "";
 
-// اجرای تابع با فشردن Enter در input
+  // افزودن آیتم‌ها به لیست
+  todoList.forEach((item) => {
+    let li = document.createElement("li");
+    li.textContent = item;
+    todoContainer.appendChild(li);
+  });
+}
+/////////////////////////////////////استفاده از توابع قبلی برای اجرا شدن ////////////////////////////////////
+// اجرای تابع برای نمایش لیست هنگام بارگذاری صفحه
+document.addEventListener("DOMContentLoaded", renderTodolist);
+addBtn.addEventListener("click", () => {
+  addItem();
+  renderTodolist();
+});
 inputValue.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
     addItem();
+    renderTodolist();
   }
 });
-
+/////////////////////////////////////////////////////////////////////////
 // tickBtn
 // trashBtn
