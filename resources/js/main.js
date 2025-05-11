8d1ec509b137277661d7ab7b5c6b82974c0e3808
alireza tabatabaeei poor <tabatabaeei.alireza@gmail.com>
2025-05-11 09:18:42 +0330

    added event enter similar addbtn to project

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,22 +1,30 @@
 //roles button To exist in this app
-var inputBtn = document.querySelector("#add");
+var addBtn = document.querySelector("#add");
 var inputValue = document.querySelector("#item");
-
-inputBtn.addEventListener("click", () => {
+function addItem() {
   let itemText = inputValue.value;
 
   if (itemText) {
-    // دریافت لیست فعلی از localStorage (اگر چیزی ذخیره شده)
     let todoList = JSON.parse(localStorage.getItem("todoItems")) || [];
-
-    // افزودن آیتم جدید به آرایه
     todoList.push(itemText);
-
-    // ذخیره‌ی آرایه‌ی جدید در localStorage
     localStorage.setItem("todoItems", JSON.stringify(todoList));
 
     console.log("لیست جدید ذخیره شد:", todoList);
+
+    // پاک کردن مقدار input
+    inputValue.value = "";
+  }
+}
+
+// اجرای تابع با کلیک روی دکمه
+addBtn.addEventListener("click", addItem);
+
+// اجرای تابع با فشردن Enter در input
+inputValue.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    addItem();
   }
 });
+
 // tickBtn
 // trashBtn
